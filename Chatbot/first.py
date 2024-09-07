78699504f459463feb2f3e2102bdd1c2326f81cd
Sumanta-Roy-1 <sumantaus@gmail.com>
2024-09-07 22:15:57 +0530

    1

--- Chatbot/first.py
+++ Chatbot/first.py
@@ -20,3 +20,6 @@ for step in range(5):
 
     # pretty print last ouput tokens from bot
     print("DialoGPT: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+
+
+    # check
