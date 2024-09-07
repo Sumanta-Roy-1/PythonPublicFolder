0b5a6592ca66589ab5538ee49b499b89ac1e41df
Sumanta-Roy-1 <sumantaus@gmail.com>
2024-09-07 22:09:13 +0530

    tested

---
+++ Test to Speech audio/2.Microsft.py
@@ -0,0 +1,16 @@
+# From https://huggingface.co/microsoft/speecht5_tts
+
+from transformers import pipeline
+from datasets import load_dataset
+import soundfile as sf
+import torch
+
+synthesiser = pipeline("text-to-speech", "microsoft/speecht5_tts")
+
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+# You can replace this embedding with your own as well.
+
+speech = synthesiser("Hello, my dog is cooler than you!", forward_params={"speaker_embeddings": speaker_embedding})
+
+sf.write("speech.wav", speech["audio"], samplerate=speech["sampling_rate"])
